ac2a93ff2968d6b744b4a4b90a5aae820556cc4c
DESKTOP-BJBE2BL\Lenovo <bennybenzigar@gmail.com>
2023-09-05 03:55:24 -0700

    'changes'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'taskmanagement';
+  constructor(){}
+  ngOnInit(): void {
+    alert(' to run the application use this command "npx json server --watch db.json --port 3000"')
+  }
 }
